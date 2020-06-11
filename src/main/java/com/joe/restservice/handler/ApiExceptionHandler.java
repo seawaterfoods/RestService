0c82614d2829b24ca7662936511c363e606d5349
seawaterfood <99390124@ss25.mcu.edu.tw>
2020-06-11 16:10:40 +0800

    異常處理 封裝錯誤信息，統一異常處理。加上註解

--- src/main/java/com/joe/restservice/handler/ApiExceptionHandler.java
+++ src/main/java/com/joe/restservice/handler/ApiExceptionHandler.java
@@ -16,30 +16,40 @@ import org.springframework.web.bind.annotation.RestControllerAdvice;
 import java.util.ArrayList;
 import java.util.List;
 
+//將此Handler置於所有@Controller前啟動
 @RestControllerAdvice
 public class ApiExceptionHandler {
 
     /*
      * 異常處理
      * 處理資源找不到。
+     * @ResponseBody返回JSON格式
      * */
     @ExceptionHandler(NotFoundException.class)
     @ResponseBody
-    public ResponseEntity<?> handlerNotFound(RuntimeException e){
+    public ResponseEntity<?> handleNotFound(RuntimeException e){
         ErrorResource errorResource = new ErrorResource(e.getMessage());
+//        返回errorResource跟404
         return new ResponseEntity<>(errorResource, HttpStatus.NOT_FOUND);
     }
     /*
      * 異常處理
      * 處理參數驗證失敗。
+     * @ResponseBody返回JSON格式
      * */
     @ExceptionHandler(InvalidRequestException.class)
     @ResponseBody
-    public ResponseEntity<?> handlerInvalidRequest(InvalidRequestException e){
+    public ResponseEntity<?> handleInvalidRequest(InvalidRequestException e){
+
+//        獲取錯誤字段集合
         Errors errors=e.getErrors();
+
         List<FieldResource> fieldResources = new ArrayList<>();
+
         List<FieldError> fieldErrors = errors.getFieldErrors();
+
         for (FieldError fieldError:fieldErrors){
+//            將fieldError內的資訊塞入自製的fieldResource中
             FieldResource fieldResource = new FieldResource(fieldError.getObjectName(),
                     fieldError.getField(),
                     fieldError.getCode(),
@@ -47,6 +57,7 @@ public class ApiExceptionHandler {
 
             fieldResources.add(fieldResource);
         }
+//      將fieldResource跟錯誤訊息塞入InvalidErrorResource中傳出
         InvalidErrorResource ier = new InvalidErrorResource(e.getMessage(),fieldResources);
         return new ResponseEntity<Object>(ier, HttpStatus.BAD_REQUEST);
     }
@@ -54,10 +65,12 @@ public class ApiExceptionHandler {
     /*
      * 異常處理
      * 處理全局異常。
+     * @ResponseBody返回JSON格式
      * */
     @ExceptionHandler(Exception.class)
     @ResponseBody
-    public ResponseEntity<?> handlerException(Exception e){
+    public ResponseEntity<?> handleException(Exception e){
+//        其餘錯誤都回傳INTERNAL_SERVER_ERROR
         return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
     }
 
